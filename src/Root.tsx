7bcf4ffb0fe2a535d5dc1d5bd0d57a19c96e6254
MarkChrisLevy <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>
2022-01-16 19:25:32 +0100

    initial commit

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,21 @@
-import {Component, h} from "@stencil/core";
+import {Component, h, Host} from "@stencil/core";
+import "app-external";
 
 @Component({
     tag: "app-root"
 })
 export class Root {
 
+    componentDidLoad() {
+        console.log("app-root did load!");
+    }
+
     render() {
-        return <div>yeee</div>
+        return <Host>
+            <div>I'm app-root, a below ⬇️ should be app-external!</div>
+            <br/>
+            <br/>
+            <app-external/>
+        </Host>
     }
 }
